b24c6dfb002b33d1f3f9fde05d6c676d9421239e
U-and-Me <yunasun0920@gmail.com>
2022-06-08 10:12:30 +0900

    #13 22-7. 소멸자 .Net Core 5 버전 이상에서는 프로그램 종료시 소멸자(종료자) 호출이 의무가 아니게 바뀐 점에 주의

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -138,6 +138,9 @@ namespace CSClass
             Console.WriteLine("두 번째 위치");
             Console.WriteLine(Sample.value);
             Console.WriteLine("세 번째 위치");
+
+            Product product = new Product("과자", 1500);
+            productA = null;
         }
     }
 
