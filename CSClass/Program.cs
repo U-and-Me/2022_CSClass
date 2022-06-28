f411efc1cd841e6511b4acf927ad8e75bc7baf2c
U-and-Me <yunasun0920@gmail.com>
2022-06-28 11:01:21 +0900

    #22 클래스 자료형 변환 - as 키워드

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -175,6 +175,12 @@ namespace CSClass
                     ((Dog) item).Bark();
                 if(item is Cat)
                     ((Cat)item).Meow();
+
+                var dog = item as Dog;
+                if(dog != null) dog.Bark();
+
+                var cat = item as Cat;
+                if(cat != null) cat.Meow();
             }
         }
     }
