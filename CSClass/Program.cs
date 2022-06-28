17ca554688de14952c42ecb379cd9e2d8fd1e482
U-and-Me <yunasun0920@gmail.com>
2022-06-28 10:54:27 +0900

    #20 #21 is 키워드

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -164,6 +164,18 @@ namespace CSClass
             Cat c = new Cat();
             Dog d = new Dog();
             
+            List<Animal> Animals = new List<Animal>() { new Dog(), new Dog(), new Dog(), new Cat(), new Cat(), new Cat() };
+
+            foreach (var item in Animals)
+            {
+                item.Eat();
+                item.Sleep();
+
+                if(item is Dog)
+                    ((Dog) item).Bark();
+                if(item is Cat)
+                    ((Cat)item).Meow();
+            }
         }
     }
 
